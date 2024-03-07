31fcbecb7dba39ff0adf26e4f67605c39731767c
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-03-07 20:31:52 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>I am a cool button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
